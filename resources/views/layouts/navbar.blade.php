c61ebeeb3d5adb4b0f5b4b5b018307a32214b1bf
Frida Ocampo <sabina.ocampo@hotmail.com>
2020-11-02 21:11:22 -0600

    contrasena

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,6 +4,7 @@
             <div class="d-flex align-items-center">
                 <a class="py-1 navbar-brand" href="/">
                     <img src="{{asset('svg/escudo_udg.svg')}}" width="35px" height="50px" alt="CUCI logo">
+                    STUDY CUCI
                 </a>
             </div>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,12 +17,16 @@
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <a href="/" tabindex="0" role="menuitem" class="dropdown-item">Habitaciones</a>
                             <a href="/" tabindex="0" role="menuitem" class="dropdown-item">Restaurantes</a>
-                            <a href="/" tabindex="0" role="menuitem" class="dropdown-item">Viajar <span class="ml-1 mt-n1 badge badge-info">New</span></a>
+                            <a href="/" tabindex="0" role="menuitem" class="dropdown-item">Antros</a>
                         </div>
                     </li>
                     <li class="nav-item"><a href="#" class="nav-link">Contacto</a></li>
+                    @if (Auth::user())
+                    <li class="nav-item"><a href="{{Auth::logout()}}" class="nav-link">Cerrar Sesion</a></li>
+                    @else
                     <li class="nav-item"><a href="{{route('login')}}" class="nav-link">Iniciar Sesión</a></li>
                     <li class="nav-item"><a href="{{route('register')}}" class="nav-link">Registrarse</a></li>
+                    @endif
                 </ul>
             </div>
         </div>
