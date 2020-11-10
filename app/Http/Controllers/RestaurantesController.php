140f8e204e12723438e1ffe8ef618bc4084337a7
Frida Ocampo <sabina.ocampo@hotmail.com>
2020-11-09 18:15:13 -0600

    Fotosagregadas

---
+++ app/Http/Controllers/RestaurantesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Restaurantes;
+use Illuminate\Http\Request;
+
+class RestaurantesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Restaurantes  $restaurantes
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Restaurantes $restaurantes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Restaurantes  $restaurantes
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Restaurantes $restaurantes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Restaurantes  $restaurantes
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Restaurantes $restaurantes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Restaurantes  $restaurantes
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Restaurantes $restaurantes)
+    {
+        //
+    }
+}
