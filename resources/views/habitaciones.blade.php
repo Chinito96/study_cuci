19b6df1d536b43aba1d1d28be78b26144e0f863b
Frida Ocampo <sabina.ocampo@hotmail.com>
2020-11-09 18:41:08 -0600

    nose que hise pero todo quedo feo

--- resources/views/habitaciones.blade.php
+++ resources/views/habitaciones.blade.php
@@ -3,13 +3,24 @@
 <div class="container">
     <div class="row">
     @foreach ($habitaciones as $habitacion)
-        <div class="col-4">
-            <a href="{{url('habitaciones/'.$habitacion->id)}}">
-                <img class="icon-rounded bg-primary-light mb-3" src="{{asset('img/casas/'.$habitacion->imagen)}}" alt="">
-            </a>
-            <h3 class="h5">{{$habitacion->descripcion}}</h3>
-            <p class="text-muted">${{$habitacion->precio}}</p>
+    <div class="col-md-4">
+        <div class="card mb-4 shadow-sm">
+            <img class="icon-rounded bg-primary-light mb-3" src="{{asset('img/casas/'.$habitacion->imagen)}}" alt="">
+          <a href="{{url('Habitaciones/'.$habitacion->id)}}">
+           
+        </a>
+        <h3 class="h5">{{$habitacion->descripcion}}</h3>
+        <p class="text-muted">${{$habitacion->precio}}</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">Ver</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Me interesa</button>
+              </div>
+              <small class="text-muted">9 mins</small>
+            </div>
+          </div>
         </div>
+      </div>
     @endforeach
     </div>
 </div>
